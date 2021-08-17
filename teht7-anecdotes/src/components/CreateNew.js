fbb36b58e8d05d6835da9ba9b6f0791ae301b4a2
mikkopaa <mikko.paajanen@metropolia.fi>
2021-08-17 14:25:04 +0300

    7.4 valmis

--- teht7-anecdotes/src/components/CreateNew.js
+++ teht7-anecdotes/src/components/CreateNew.js
@@ -1,43 +1,41 @@
 import React from 'react'
-import { useState } from 'react'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-  console.log(props)
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      props.addNew({
-        content,
-        author,
-        info,
-        votes: 0
-      })
-    }
-  
-    return (
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-          </div>
-          <div>
-            author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-          </div>
-          <button>create</button>
-        </form>
-      </div>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
   }
-    
+  
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
 
 export default CreateNew
