bc479ecc52e2019ef16045e2da2ebc5e733345ca
mikkopaa <mikko.paajanen@metropolia.fi>
2022-10-27 10:52:09 +0300

    Ex9.3 finished

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -8,6 +8,29 @@ interface ExerciseResults {
   average: number
 }
 
+interface ExerciseValues {
+  target: number,
+  hours: Array<number>
+}
+
+const parseArgs = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments.');
+
+  const hours = args.splice(2, args.length - 3).map(hours => Number(hours))
+  hours.map(value => {
+    if (isNaN(value)) throw new Error('Exercise values must be numbers.');
+    
+  })
+  if (!isNaN(Number(args[2]))) {
+    return {
+      target: Number(args[2]),
+      hours
+    }
+  } else {
+    throw new Error('Target value was not a number.');
+  }
+}
+
 const ratings = [
   { rating: 1, description: 'You should exercise more!' },
   { rating: 2, description: 'You\'ve met your target.' },
@@ -33,4 +56,14 @@ const calculateExercises = (exerciseHours: Array<number>, target: number): Exerc
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { target, hours } = parseArgs(process.argv)
+  // console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+  console.log(calculateExercises(hours, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage)
+}
