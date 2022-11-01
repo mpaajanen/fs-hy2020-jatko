994fe8af81c3010081342b0c0199f5cfee5980d8
mikkopaa <mikko.paajanen@metropolia.fi>
2022-11-01 11:08:42 +0200

    Ex9.12 finished

--- patentior_part9/src/services/patientService.ts
+++ patentior_part9/src/services/patientService.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { v1 as uuid } from 'uuid';
 import patientData from '../../data/patients';
 
-import { Patient } from '../types';
+import { Patient, NewPatient } from '../types';
 
 const patients: Array<Patient> = patientData;
 
@@ -18,7 +21,18 @@ const getEntriesNoSsn = (): Omit<Patient, 'ssn'>[] => {
   }));
 };
 
+const addPatient = (entry: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...entry
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getEntries,
-  getEntriesNoSsn
+  getEntriesNoSsn,
+  addPatient
 };
