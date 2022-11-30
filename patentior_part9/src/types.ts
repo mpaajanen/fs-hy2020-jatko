2a7edbaf7abdd0cbc5168a178ef1b3370dfea197
mikkopaa <mikko.paajanen@metropolia.fi>
2022-11-30 13:06:30 +0200

    Ex9.19 finished

--- patentior_part9/src/types.ts
+++ patentior_part9/src/types.ts
@@ -22,8 +22,8 @@ export enum Gender {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+// export interface Entry {
+// }
 
 export interface Patient {
   id: string;
@@ -34,5 +34,44 @@ export interface Patient {
   dateOfBirth: string;
   entries: Entry[]
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+export type Entry = | HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
